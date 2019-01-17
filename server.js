a659aa68abc334847e21cd14a44eaf74cb1e138c
talking-buffalo <angela.mr.lee@gmail.com>
2019-01-17 15:48:09 -0700

    learningwhereitis

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-$("#enter").on("click", function (){
-    points.starts();
-})
+$("#button").on("click", function (){
+    button.start();
+});
 
 var button = {
     start: function () {
-        $("#subwrapper").prepend('<h2> Push it real good </h2>');
-        $("#enter").remove();
+        $("#button").remove();
+        $("#subwrapper").prepend('<h2> Push it real good! </h2>');
     }
 };
+
